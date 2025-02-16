b916b3e82e589b4407d84d7df0a7ea126a35d4b5
woohyun23 <parkwh0823@gmail.com>
2025-02-17 02:02:24 +0900

    실패 테스트 코드 추가

--- src/test/java/hello/core/discount/RateDiscountPolicyTest.java
+++ src/test/java/hello/core/discount/RateDiscountPolicyTest.java
@@ -15,7 +15,6 @@ class RateDiscountPolicyTest {
     @Test
     @DisplayName("VIP는 10% 할인이 적용되어야 한다")
     void vip_o() {
-
         //given
         Member member = new Member(1L, "memberVIP", Grade.VIP);
 
@@ -25,5 +24,16 @@ class RateDiscountPolicyTest {
         //then
         Assertions.assertThat(discount).isEqualTo(1000);
     }
+    @Test
+    @DisplayName("VIP는 10% 할인이 적용되지 않아야 한다")
+    void vip_x() {
+        //given
+        Member member = new Member(2L, "memberBASIC", Grade.BASIC);
 
+        //when
+        int discount = discountPolicy.discount(member, 10000);
+
+        //then
+        Assertions.assertThat(discount).isEqualTo(0);
+    }
 }
