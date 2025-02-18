923409c3bb8ee5c91b303777bafce8e55a01f7d1
woohyun23 <parkwh0823@gmail.com>
2025-02-18 15:57:47 +0900

    의존성 주입에 따른 Test code 수정

--- src/test/java/hello/core/member/MemberServiceTest.java
+++ src/test/java/hello/core/member/MemberServiceTest.java
@@ -1,11 +1,20 @@
 package hello.core.member;
 
+import hello.core.AppConfig;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class MemberServiceTest {
 
-    MemberService memberService = new MemberServiceImpl();
+    MemberService memberService;
+
+    @BeforeEach
+    public void beforeEach(){
+        AppConfig appconfig = new AppConfig();
+        memberService = appconfig.memberService();
+    }
+
     @Test
     void join() {
         //given
