be99be2a1ba6bc9d602dd8b107d73a90bcc55193
woohyun23 <parkwh0823@gmail.com>
2025-02-18 15:49:31 +0900

    Revert "OrderApp 의존성 주입 오류 수정"
    This reverts commit dfa4dee4c5380ac35aa5806f710cc9fc8f293b4f.

--- src/main/java/hello/core/OrderApp.java
+++ src/main/java/hello/core/OrderApp.java
@@ -11,9 +11,8 @@ import hello.core.order.OrderServiceImpl;
 public class OrderApp {
 
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
-        OrderService orderService = appConfig.orderService();
+        MemberService memberService = new MemberServiceImpl(null);
+        OrderService orderService = new OrderServiceImpl(null, null);
 
         Long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
