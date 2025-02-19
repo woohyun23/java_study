202ebec890db0c2b1fac05a871c6f0bdfe9605f5
woohyun23 <parkwh0823@gmail.com>
2025-02-20 01:03:34 +0900

    spring bean 적용

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -2,19 +2,25 @@ package hello.core;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 // AppConfig : 애플리케이션 전체를 구성하고 설정한다는 의미를 가지는 클래스
 // 애플리케이션의 실제 동작에 필요한 구현 객체를 생성
 // 생성한 객체 인스턴스의 참조(레퍼런스)를 생성자를 통해 주입(연결)
 // MemberServiceImpl -> MemoryMemberRepository
 // OrderServiceImpl  -> MemoryMemberRepository, FixDiscountPolicy
+
+@Configuration
 public class AppConfig {
 
+    @Bean
     public MemberService memberService() {
         // 추상화에 의존하여 MemberServiceImpl 클래스에서
         // MemoryMemberRepository를 구성하는 것이 아닌
@@ -23,16 +29,21 @@ public class AppConfig {
         return new MemberServiceImpl(memberRepository());
     }
 
-    private static MemoryMemberRepository memberRepository() {
+    @Bean
+    public static MemoryMemberRepository memberRepository() {
         // DB를 변경하게 되면, 이 부분만 수정하면 수정 완료
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService() {
-        return new OrderServiceImpl(memberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {
-        return new FixDiscountPolicy();
+        // 고정 금액 할인 -> 할인율 적용을 변경
+        // return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 }
