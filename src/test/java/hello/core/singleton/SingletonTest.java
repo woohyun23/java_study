318b5f046e6988e24b59f64074bff891d3dd7b9e
woohyun23 <parkwh0823@gmail.com>
2025-03-07 16:29:02 +0900

    Spring Singleton 적용하여 객체 생성

--- src/test/java/hello/core/singleton/SingletonTest.java
+++ src/test/java/hello/core/singleton/SingletonTest.java
@@ -5,23 +5,25 @@ import hello.core.member.MemberService;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class SingletonTest {
 
     @Test
     @DisplayName("스프링 없는 순수한 DI 컨테이너")
     void pureContainer() {
-        AppConfig appConfig = new AppConfig();
-        // 1. 조회 : 호출 시, 객체를 생성
-        MemberService memberService1 = appConfig.memberService();
 
-        // 2. 조회 : 호출 시, 객체를 생성
-        MemberService memberService2 = appConfig.memberService();
+//        AppConfig appConfig = new AppConfig();
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberService memberService1 = ac.getBean("memberService", MemberService.class);
+        MemberService memberService2 = ac.getBean("memberService", MemberService.class);
 
         System.out.println("memberService1 = " + memberService1);
         System.out.println("memberService2 = " + memberService2);
 
         // memberService1 != memberService2
-        Assertions.assertThat(memberService1).isNotSameAs(memberService2);
+        Assertions.assertThat(memberService1).isSameAs(memberService2);
     }
 }
